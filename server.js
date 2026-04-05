7410c44d19c2e8f8bd4e0ddb3f97f9790bc55fc9
Hassibur Rahman <md9178558@gmail.com>
2026-04-05 12:48:31 +0600

    final

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import { clerkMiddleware, requireAuth } from '@clerk/express'
+import aiRouter from "./routes/aiRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+
+
 
 const app = express();
+
+await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
@@ -12,6 +18,8 @@ app.use(clerkMiddleware())
 app.get("/", (req, res) => res.send('Server is Live!'));
 
 app.use(requireAuth())
+
+app.use("/api/ai", aiRouter)
 
 const PORT = process.env.PORT || 3000;
 
